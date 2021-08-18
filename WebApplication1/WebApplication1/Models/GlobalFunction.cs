ce8b1a195d1a293d7e46eb2858b6fcf16f747a5d
Yaowen <yaowen@tkpos.com.tw>
2021-08-18 11:20:06 +0800

    ++
    ++

--- WebApplication1/WebApplication1/Models/GlobalFunction.cs
+++ WebApplication1/WebApplication1/Models/GlobalFunction.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace WebApplication1.Models
 {
     public class GlobalFunction
     {
-        public static string GlobalConnString = ConfigurationManager.ConnectionStrings["mysqlconnection"].ConnectionString;                    //資料庫連接字串
+        public static string GlobalConnString = WebConfigurationManager.ConnectionStrings["mysqlconnection"].ConnectionString;                    //資料庫連接字串
     }
 }
